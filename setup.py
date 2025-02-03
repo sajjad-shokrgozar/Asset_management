c1279699f75b56af0df8b0b3605edc1c0f9d617a
sajjad <shokrgozarsajjad@gmail.com>
2025-02-03 22:52:38 +0330

    version to 0.0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="portfolio_management",
+    name="asset_management",
     version="0.1.0",
     author="sajjad_shokrgozar",
     author_email="shokrgozarsajjad@gmail.com",
